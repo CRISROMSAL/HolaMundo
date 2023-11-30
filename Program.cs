9fdaab521e883efe831b1115d10617b7e780b7ec
Carlos Basulto Pardo <carlosbasultopardo@gmail.com>
2023-11-30 23:10:37 +0100

    Hi

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("¡Hola, Mundo!");
+Console.WriteLine("Bonjour, le Monde!");
+Console.WriteLine("Hallo, Welt!");
+Console.WriteLine("Ciao, Mondo!");
+Console.WriteLine("Olá, Mundo!");
+Console.WriteLine("你好，世界！");
+Console.WriteLine("こんにちは、世界！");
+Console.WriteLine("Привет, мир!");
+Console.WriteLine("مرحبًا، العالم!");
+Console.WriteLine("नमस्ते, दुनिया!");
+Console.WriteLine("হ্যালো, বিশ্ব!");
+Console.WriteLine("Marhaban, Al-Alam!");
+Console.WriteLine("안녕하세요, 세계!");
+Console.WriteLine("Merhaba, Dünya!");
+Console.WriteLine("Hallo, Wereld!");
+Console.WriteLine("Hej, Världen!");
+Console.WriteLine("Γειά σου, Κόσμε!");
+Console.WriteLine("שלום, עולם!");
+Console.WriteLine("Witaj, Świecie!");
